c606e19a1ebb9bab81d69d057356308cd4ccd6ee
Sam Krishna <sam.krishna@gmail.com>
2011-03-16 12:05:42 -0400

    Added Zarra Core Data book project and Chapter 2 tutorial start.

---
+++ Zarra-CoreData/Chapter02/GrokkingRecipes/GrokkingRecipes_AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  GrokkingRecipes_AppDelegate.h
+//  GrokkingRecipes
+//
+//  Created by Sam Krishna on 3/16/11.
+//  Copyright SectorMobile 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GrokkingRecipes_AppDelegate : NSObject 
+{
+    NSWindow *window;
+    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ Zarra-CoreData/Chapter02/GrokkingRecipes/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GrokkingRecipes
+//
+//  Created by Sam Krishna on 3/16/11.
+//  Copyright SectorMobile 2011. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}

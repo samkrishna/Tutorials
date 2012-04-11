99c43c456d7cee7e23f7d9e4aa30e3c5b23890cb
Sam Krishna <sam.krishna@gmail.com>
2012-04-11 07:40:26 -0400

    Got Main Window to load.

---
+++ BNR-iOS-2012/Chapter01/Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Sam Krishna on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UILabel *questionField;
+@property (strong, nonatomic) IBOutlet UILabel *answerField;
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end

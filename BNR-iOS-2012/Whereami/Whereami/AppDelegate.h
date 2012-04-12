79c682423f69a8ed7ae9ce1de83df248bbcf34d7
Sam Krishna <sam.krishna@gmail.com>
2012-04-12 06:21:05 -0400

    Created and Modified Whereami to use Main Window xib.

---
+++ BNR-iOS-2012/Whereami/Whereami/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Whereami
+//
+//  Created by Sam Krishna on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
